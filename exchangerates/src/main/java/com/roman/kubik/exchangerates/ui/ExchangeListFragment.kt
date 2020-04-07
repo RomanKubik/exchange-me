ca93743ac482fafb51526001188f63bfe06ba5f9
Roman Kubik <roman.kubik@appsfactory.de>
2020-04-07 13:49:25 +0200

    [ADD] Added exchange rates layout

--- exchangerates/src/main/java/com/roman/kubik/exchangerates/ui/ExchangeListFragment.kt
+++ exchangerates/src/main/java/com/roman/kubik/exchangerates/ui/ExchangeListFragment.kt
@@ -1,9 +1,13 @@
 package com.roman.kubik.exchangerates.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.roman.kubik.exchangeme.activityComponent
 import com.roman.kubik.exchangeme.dagger.viewModel
+import com.roman.kubik.exchangerates.R
 import com.roman.kubik.exchangerates.dagger.DaggerExchangeListComponent
 
 /**
@@ -13,6 +17,14 @@ class ExchangeListFragment : Fragment() {
 
     private val viewModel by viewModel {
         DaggerExchangeListComponent.factory().create(activityComponent).exchangeListViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_exchange_list, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
